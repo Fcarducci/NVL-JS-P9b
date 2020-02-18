a949938debe0ee6c11aab0f1872696de6a98687f
Fcarducci <fcarducci20@gmail.com>
2020-02-18 14:14:52 +0100

    array

---
+++ p9b.js
@@ -0,0 +1,5 @@
+var dias = 'lunes, martes, miercoles, jueves, viernes, sabado, domingo';
+var ciudades = 'Madrid, Sevilla, Barcelona, Valencia, Zaragoza, Bilbao, Donostia';
+
+var conciertos = [['lunes', 'Madrid'], ['martes', 'Sevilla'], ['miercoles', 'Barcelona'], ['jueves', 'Valencia'], ['viernes', 'Zaragoza'], ['sabado', 'Bilbao'], ['domingo', 'Donostia']];
+
